863453fd3ceda6f28bc630c5975ab5b1f0af18b1
Denis Kuznetsov <kuznetsov.den.p@gmail.com>
2024-08-22 12:51:32 +0300

    dev: upd

---
+++ src/run.sh
@@ -0,0 +1,5 @@
+source scene_graph/venv/bin/activate
+python scene_graph/script.py
+cp scene_graph/ouput.json rendering/input.json
+source rendering/venv/bin/activate
+python rendering/script.py
---
+++ src/telegram_ui/README.md
@@ -0,0 +1,4 @@
+```bash
+source venv/bin/activate
+python script.py
+```
